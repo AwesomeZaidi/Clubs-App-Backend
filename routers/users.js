9b629e95314da0dd620944d4ef563efed93ff35f
Asim Zaidi <asimzaidih@gmail.com>
2019-02-09 18:24:52 -0800

    user post routes

--- routers/users.js
+++ routers/users.js
@@ -1,10 +1,23 @@
+// USERS ROUTERS: contains all the routes needed for users and calls their functions using async/await.
+//  login : GET, POST
+//  signup : GET, POST
+//  logout : GET
+
 const users = require('express').Router();
 
 router.get('/login', (req,res) => {
     // login function
 });
 
+router.post('/login', (req,res) => {
+    // login function
+});
+
 router.get('/signup', async (req,res) => {
+    // signup function
+});
+
+router.post('/signup', async (req,res) => {
     // signup function
 });
 

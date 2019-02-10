2476d742fc713f999b20fea0345ce3e4d82a5ac2
Asim Zaidi <asimzaidih@gmail.com>
2019-02-09 18:24:39 -0800

    user routes

--- routers/users.js
+++ routers/users.js
@@ -4,4 +4,12 @@ router.get('/login', (req,res) => {
     // login function
 });
 
+router.get('/signup', async (req,res) => {
+    // signup function
+});
+
+router.get('/logout', async (req,res) => {
+    // logout function
+});
+
 module.exports = users;

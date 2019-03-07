9c3c45b8ddb4cda7fa78484cf96a1f912fa55caf
Asim Zaidi <asimzaidih@gmail.com>
2019-03-07 00:58:56 -0800

    fixed checkAuth middleware

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,16 +1,15 @@
-// const User = require('../models/user');
-// const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-// module.exports = function (req, res, next) {
-//     if (req.cookies && req.cookies.nToken) {
-//         const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
-//         User.findById(uid).then(user => {
-//             req.user = user;
-//             res.locals.authenticatedUser = user;
-//             return next();
-//         });
-//     } else {
-//         res.user = null;
-//         return next();
-//     }
-// }
+module.exports = function (req, res, next) {
+    console.log("middleware req:", req);    
+    if (req.cookies && req.cookies.nToken) {
+        const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
+        User.findById(uid).then(user => {
+            req.user = user;
+            return next();
+        });
+    } else {
+        return res.status(401).send("User not logged in");
+    };
+};

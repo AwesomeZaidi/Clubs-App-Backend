16fbde60a64996a4dbb1dbde4e0ebd26dd53f2c4
Asim Zaidi <asimzaidih@gmail.com>
2019-03-17 16:16:49 -0500

    small, temporary changes to model

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,7 +2,6 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    console.log("middleware req:", req);    
     if (req.cookies && req.cookies.nToken) {
         const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
         User.findById(uid).then(user => {
--- models/event.js
+++ models/event.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
   title: { type: String },
-  date: { type: Date },
   club: [{ type: Schema.Types.ObjectId, ref: "Club" }],
 }, {
   timestamps: true,

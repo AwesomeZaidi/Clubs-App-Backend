8fc03c80dabd7252dd491e844c0d9654ffcabb3f
Asim Zaidi <asimzaidih@gmail.com>
2019-04-25 22:37:47 -0700

    refactored middleware again

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,6 +1,20 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
+function getUser (req, res, next) {
+    return new Promise((resolve, reject) => {
+        if (req.cookies && req.cookies.nToken) {
+            const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
+            User.findById(uid).then(user => {                
+                req.user = user;
+                resolve(user);
+            });
+        } else {
+            reject("User not logged in");
+        };
+    });
+};
+
 function checkAuth (req, res, next) {
     if (req.cookies && req.cookies.nToken) {
         const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
@@ -13,39 +27,26 @@ function checkAuth (req, res, next) {
     };
 };
 
-function checkLeader (req, res, next) {
-    if (req.cookies && req.cookies.nToken) {
-        const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
-        User.findById(uid).then(user => {
-            req.user = user;
-            if (req.user.type === 'leader') {
-                console.log('leader authenticated.');
-                
-                return next();
-            };
-            return res.status(401).send("Leader user not found");
-        }).catch((err) => {
-            return res.status(500).send("Something went wrong, user not found.");
-        });
-    } else {
-        return res.status(401).send("User not logged in");
-    };
+async function checkLeader (req, res, next) {
+    // const user = await getUser(req, res);
+    // if (user.type === 'leader') {
+    //     return next();
+    // } else {
+    //     return res.status(500).send("Something went wrong, user not found, please relogin.");
+    // };
+    getUser(req, res).then((user) => {
+        return next();
+    }).catch(err => {  
+        return res.status(500).send("Something went wrong, user not found, please relogin.");       
+    });
 };
 
-function checkAdmin (req, res, next) {
-    if (req.cookies && req.cookies.nToken) {
-        const uid = jwt.decode(req.cookies.nToken, process.env.SECRET)._id;
-        User.findById(uid).then(user => {
-            req.user = user;
-            if (req.user.type === 'admin') {
-                return next();
-            };
-            return res.status(401).send("Admin user not found");
-        }).catch((err) => {
-            return res.status(500).send("Something went wrong, user not found.");
-        });
+async function checkAdmin (req, res, next) {
+    const user = await getUser(req, res);
+    if (user.type === 'admin') {
+        return next();
     } else {
-        return res.status(401).send("User not logged in");
+        return res.status(500).send("Something went wrong, user not found, please relogin.");
     };
 };
 
